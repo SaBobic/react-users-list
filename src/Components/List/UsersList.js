067a5d48d064575c90e16ffc1ffd7e517e1c1ed9
SaBobic <fabrizioettori@gmail.com>
2022-11-17 12:07:59 +0100

    added toCapitalize function into UsersList

--- src/Components/List/UsersList.js
+++ src/Components/List/UsersList.js
@@ -3,11 +3,16 @@ import UserItem from './UserItem';
 import styles from './UsersList.module.css';
 
 const UserList = props => {
+    const toCapitalize = word => {
+        word = word[0].toUpperCase() + word.substr(1);
+        return word;
+    };
+
     return (
         <Card className={styles.users}>
             <ul>
                 {props.users.map(user => {
-                    return <UserItem key={user.id} username={user.username} age={user.age} />;
+                    return <UserItem key={user.id} username={toCapitalize(user.username)} age={user.age} />;
                 })}
             </ul>
         </Card>
