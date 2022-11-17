5986e79f8a71fa5026a3daef1737b268a85956cb
SaBobic <fabrizioettori@gmail.com>
2022-11-17 11:35:53 +0100

    added portal for FormAlert

--- src/Components/Form/FormAlert.js
+++ src/Components/Form/FormAlert.js
@@ -1,26 +1,33 @@
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import styles from './FormAlert.module.css';
+import ReactDOM from 'react-dom';
+
+const Backdrop = props => {
+    return <div onClick={props.onClose} className={styles.backdrop}></div>;
+};
+
+const AlertOverlay = props => {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={props.onClose}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
 
 const FormAlert = props => {
-    const closeHandler = () => {
-        props.onClose();
-    };
-
     return (
         <>
-            <div onClick={closeHandler} className={styles.backdrop}></div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={closeHandler}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<AlertOverlay onClose={props.onClose} title={props.title} message={props.message} />, document.getElementById('overlay-root'))}
         </>
     );
 };
